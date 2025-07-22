5f1836ca3a36fbffdfbdbbd081da7dd556225f66
Mohammed Baba <mbfmz20@gmail.com>
2025-07-22 16:50:50 +0100

    ajout le model user

---
+++ Backend/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    countryCode: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    // Optionally, add fields like role, isVerified, etc.
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
